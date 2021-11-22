e135636554aeb93c76455fc34e35829257c09369
Camilo A <ing_camiloacg@hotmail.com>
2021-11-22 16:23:59 -0500

    Crea ejemplo de sharedPreferences

--- app/src/main/java/com/misiontic/holamundo05/LoginActivity.java
+++ app/src/main/java/com/misiontic/holamundo05/LoginActivity.java
@@ -2,7 +2,9 @@ package com.misiontic.holamundo05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,11 +12,14 @@ import android.widget.EditText;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText etName;
+    private SharedPreferences settings; // SP
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        settings = getSharedPreferences("id", Context.MODE_PRIVATE); //SP
     }
 
     public void goToMain(View view) {
@@ -24,6 +29,13 @@ public class LoginActivity extends AppCompatActivity {
         Intent intentMain = new Intent(this, MainActivity.class);
 
         intentMain.putExtra("user", name);
+
+        //SP
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("id", 1);
+            editor.putString("usuario", name);
+            editor.commit();
+        //
 
         startActivity(intentMain);
     }
--- app/src/main/java/com/misiontic/holamundo05/OrderActivity.java
+++ app/src/main/java/com/misiontic/holamundo05/OrderActivity.java
@@ -2,6 +2,8 @@ package com.misiontic.holamundo05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +12,14 @@ import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
+    private SharedPreferences settings; // SP
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        settings = getSharedPreferences("id", Context.MODE_PRIVATE); //SP
 
         Button btnOrder = findViewById(R.id.btnOrder);
         btnOrder.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +31,9 @@ public class OrderActivity extends AppCompatActivity {
     }
 
     public void realizarPedido() {
+
+        String usuario = settings.getString("usuario", "Pepe");
+
         CheckBox cbPollo = findViewById(R.id.cbPollo);
         CheckBox cbSalami = findViewById(R.id.cbSalami);
         CheckBox cbJamon = findViewById(R.id.cbJamon);
@@ -41,6 +50,8 @@ public class OrderActivity extends AppCompatActivity {
         if (cbJamon.isChecked()) {
             strPedido = strPedido.concat("jamón ");
         }
+
+        strPedido = strPedido.concat(" para el señor(a) " + usuario);
 
         Toast.makeText(this, strPedido, Toast.LENGTH_LONG).show();
     }
