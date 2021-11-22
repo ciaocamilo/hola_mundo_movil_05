d31f33351693d47e74919e652b1c680e0bb98b10
Camilo A <ing_camiloacg@hotmail.com>
2021-11-22 17:36:05 -0500

    Crea conexión a base de datos SQLite

--- app/src/main/java/com/misiontic/holamundo05/PersonFormActivity.java
+++ app/src/main/java/com/misiontic/holamundo05/PersonFormActivity.java
@@ -3,6 +3,9 @@ package com.misiontic.holamundo05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.misiontic.holamundo05.db.MySQLiteHelper;
 
 public class PersonFormActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class PersonFormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_form);
     }
+
+    public void guardarFormulario(View view) {
+        MySQLiteHelper conexion_bd = new MySQLiteHelper(this);
+        // Error en el Query
+        /*String insert_query = "INSERT INTO personas (nombres, apellidos, direccion, telefono, fecha_nacimiento) " +
+                "VALUES ('Juan', 'Castro', 'Avenida Y', '31517454', '05/08/2005')";
+        conexion_bd.insertData(insert_query);*/
+    }
+
 }
---
+++ app/src/main/java/com/misiontic/holamundo05/db/MySQLiteHelper.java
@@ -0,0 +1,37 @@
+package com.misiontic.holamundo05.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "app_05_db.sqlite";
+    private static final int DB_VERSION = 1;
+
+    private static final String PEOPLE_TABLE_CREATE = "CREATE TABLE personas(_id PRIMARY KEY AUTOINCREMENT, " +
+                                                        "nombres TEXT, apellidos TEXT, direccion TEXT, telefono TEXT, " +
+                                                        "fecha_nacimiento DATE, foto TEXT)";
+
+
+    public MySQLiteHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(PEOPLE_TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+
+    public void insertData(String sentence) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(sentence);
+    }
+
+}
