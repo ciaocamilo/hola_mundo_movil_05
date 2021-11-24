5985860f122e998827e76cb043fb0c2ebc90df2b
Camilo A <ing_camiloacg@hotmail.com>
2021-11-24 15:02:31 -0500

    Agrega insert de persona

--- app/src/main/java/com/misiontic/holamundo05/PersonFormActivity.java
+++ app/src/main/java/com/misiontic/holamundo05/PersonFormActivity.java
@@ -4,22 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.misiontic.holamundo05.db.MySQLiteHelper;
 
 public class PersonFormActivity extends AppCompatActivity {
 
+    private EditText etName;
+    private EditText etLastname;
+    private EditText etAddress;
+    private EditText etPhone;
+    private EditText etBirthday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_form);
+
+        etName = findViewById(R.id.etFormPersonName);
+        etLastname = findViewById(R.id.etFormLastname);
+        etAddress = findViewById(R.id.etFormAddress);
+        etPhone = findViewById(R.id.etTelephone);
+        etBirthday = findViewById(R.id.etBirthday);
     }
 
     public void guardarFormulario(View view) {
+        String name = etName.getText().toString();
+        String lastname = etLastname.getText().toString();
+        String address = etAddress.getText().toString();
+        String phone = etPhone.getText().toString();
+        String birthday = etBirthday.getText().toString();
+
         MySQLiteHelper conexion_bd = new MySQLiteHelper(this);
-        /*String insert_query = "INSERT INTO personas (nombres, apellidos, direccion, telefono, fecha_nacimiento) " +
-                "VALUES ('"+name+"', '"+lastname+"', '"+address+"', '"+phone+"', '"+birthday+"')";
-        conexion_bd.insertData(insert_query);*/
+        String insert_query = "INSERT INTO personas (nombres, apellidos, direccion, telefono, fecha_nacimiento) " +
+                "VALUES ('" + name + "', '" + lastname + "', '" + address + "', '" + phone + "', '" + birthday + "')";
+
+        boolean success = conexion_bd.insertData(insert_query);
+        if (success) {
+            Toast.makeText(this, "Persona guardada con éxito", Toast.LENGTH_LONG).show();
+            limpiarFormulario();
+        } else {
+            Toast.makeText(this, "Error al guardar la persona", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void limpiarFormulario(View view) {
+        etName.setText("");
+        etLastname.setText("");
+        etAddress.setText("");
+        etPhone.setText("");
+        etBirthday.setText("");
+    }
+
+    public void limpiarFormulario() {
+        etName.setText("");
+        etLastname.setText("");
+        etAddress.setText("");
+        etPhone.setText("");
+        etBirthday.setText("");
     }
 
 }
--- app/src/main/java/com/misiontic/holamundo05/db/MySQLiteHelper.java
+++ app/src/main/java/com/misiontic/holamundo05/db/MySQLiteHelper.java
@@ -10,7 +10,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "app_05_db.sqlite";
     private static final int DB_VERSION = 1;
 
-    private static final String PEOPLE_TABLE_CREATE = "CREATE TABLE personas(_id PRIMARY KEY AUTOINCREMENT, " +
+    private static final String PEOPLE_TABLE_CREATE = "CREATE TABLE personas(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                                                         "nombres TEXT, apellidos TEXT, direccion TEXT, telefono TEXT, " +
                                                         "fecha_nacimiento DATE, foto TEXT)";
 
@@ -29,9 +29,16 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
     }
 
-    public void insertData(String sentence) {
-        SQLiteDatabase db = getWritableDatabase();
-        db.execSQL(sentence);
+    public boolean insertData(String sentence) {
+        boolean success = false;
+        try {
+            SQLiteDatabase db = getWritableDatabase();
+            db.execSQL(sentence);
+            success = true;
+        } catch (Exception e) {
+            success = false;
+        }
+        return success;
     }
 
 }
