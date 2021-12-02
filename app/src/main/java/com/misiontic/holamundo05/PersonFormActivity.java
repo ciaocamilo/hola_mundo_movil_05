d237ed4c9d82ff281511e7bf24b6275cf096f73c
Camilo A <ing_camiloacg@hotmail.com>
2021-12-02 15:20:54 -0500

    Agrega función de guardar imagen (revisar)

--- app/src/main/java/com/misiontic/holamundo05/PersonFormActivity.java
+++ app/src/main/java/com/misiontic/holamundo05/PersonFormActivity.java
@@ -5,11 +5,17 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
@@ -17,6 +23,9 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.misiontic.holamundo05.db.MySQLiteHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class PersonFormActivity extends AppCompatActivity {
 
@@ -66,6 +75,10 @@ public class PersonFormActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Error al guardar la persona", Toast.LENGTH_LONG).show();
         }
+
+        // Tests save
+        saveToGallery();
+
     }
 
     public void limpiarFormulario(View view) {
@@ -108,6 +121,39 @@ public class PersonFormActivity extends AppCompatActivity {
                 }
             });
 
+
+    private void saveToGallery() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Bitmap bitmap = ((BitmapDrawable) ivPictureForm.getDrawable()).getBitmap();
+
+            FileOutputStream outputStream = null;
+            File file = Environment.getExternalStorageDirectory();
+            File dir = new File(file.getAbsolutePath() + "/Fotos_05");
+            dir.mkdirs();
+
+            String filename = String.format("%d.png", System.currentTimeMillis());
+            File outfile = new File (dir, filename);
+
+            try {
+                outputStream = new FileOutputStream(outfile);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                outputStream.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            ActivityCompat.requestPermissions(PersonFormActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 44);
+        }
+    }
 
 
 }
